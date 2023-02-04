d8687c1b4691bdb12085ff48bc0579df611d6ff7
henriits <121551949+henriits@users.noreply.github.com>
2023-02-04 20:48:12 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route('/api/v1/<word>/')
+def api(word):
+    definition = word.upper()
+    result_dictionary = {
+        "word": word,
+        "description": definition
+    }
+    return result_dictionary
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
+page = "http://127.0.0.1:5001/api/v1/flower"
